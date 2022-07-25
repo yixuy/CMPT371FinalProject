5d9849a1a5a3cd890a26113efdc144587fba4f1c
Praneer Shrestha <praneer.shrest@gmail.com>
2022-07-25 16:40:28 -0700

    pretty print Board

--- GameLogic/Board.py
+++ GameLogic/Board.py
@@ -26,4 +26,8 @@ class Board:
         self.board = newBoard
 
     def printBoard(self):
-        print('\n'.join(' '.join(str(i) for i in row) for row in self.board))
+        print('\n'.join(' '.join('{0: ^3}'.format(str(i)) for i in row) for row in self.board))
+
+board = Board(10, 10)
+board.initializeBoard()
+board.printBoard()
