a651cc1e48b7f096e3e63e2cba006ac9f19b7f48
Denzel Tjokroardi <dtjokroa@gmail.com>
2022-07-28 17:55:13 -0700

    added 1x2 and 2x1 walls

--- GameLogic/Board.py
+++ GameLogic/Board.py
@@ -22,7 +22,7 @@ class Board:
     # Cell Type 0: 1x1 cell
     # Cell Type 1: 1x2 cell
     # Cell Type 2: 2x1 cell
-    def setCell(self, x_coord, y_coord):
+    def setCellType0(self, x_coord, y_coord):
         self.board[y_coord+1][x_coord] = 0
         self.board[y_coord][x_coord+1] = 0
         self.board[y_coord-1][x_coord] = 0
@@ -31,6 +31,28 @@ class Board:
         self.board[y_coord-1][x_coord-1] = 0
         self.board[y_coord+1][x_coord-1] = 0
         self.board[y_coord-1][x_coord+1] = 0
+
+    def setCellType1(self, x_coord, y_coord):
+        self.board[y_coord+1][x_coord] = -1
+        self.board[y_coord+2][x_coord] = 0
+        self.board[y_coord][x_coord+1] = 0
+        self.board[y_coord-1][x_coord] = 0
+        self.board[y_coord][x_coord-1] = 0
+        self.board[y_coord+2][x_coord+1] = 0
+        self.board[y_coord-1][x_coord-1] = 0
+        self.board[y_coord+2][x_coord-1] = 0
+        self.board[y_coord-1][x_coord+1] = 0
+
+    def setCellType2(self, x_coord, y_coord):
+        self.board[y_coord][x_coord + 1] = -1
+        self.board[y_coord+1][x_coord] = 0
+        self.board[y_coord][x_coord+2] = 0
+        self.board[y_coord-1][x_coord] = 0
+        self.board[y_coord][x_coord-1] = 0
+        self.board[y_coord+1][x_coord+2] = 0
+        self.board[y_coord-1][x_coord-1] = 0
+        self.board[y_coord+1][x_coord-1] = 0
+        self.board[y_coord-1][x_coord+2] = 0
 
     def getBoard(self):
         return  self.board;
@@ -42,13 +64,20 @@ class Board:
         self.board = [[0 for i in range(self.height)] for j in range(self.width)]
         n_walls = int(self.height * self.width * 0.2)
         for i in range(n_walls):
-            # cellType = random.randint(0,2)
+            cellType = random.randint(0,2)
             random_Y = random.randint(0, self.width-1)
             random_X = random.randint(0, self.height-1)
             self.board[random_Y][random_X] = -1
             if (random_Y//2 != 0):
                 if(random_Y != 0 and random_X != 0 and random_Y != self.height-1 and random_X != self.width-1):
-                    self.setCell(random_X, random_Y)
+                    if(cellType == 0):
+                        self.setCellType0(random_X, random_Y)
+                    elif(cellType == 1 and random_Y < self.height-2):
+                        self.setCellType1(random_X,random_Y)
+                    elif(cellType == 2 and random_X < self.width-2):
+                        self.setCellType2(random_X, random_Y)
+
+
 
     def printBoard(self):
         print('\n'.join(' '.join('{0: ^3}'.format(str(i)) for i in row) for row in self.board))
