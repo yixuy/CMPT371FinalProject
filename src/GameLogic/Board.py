100035c8f14ba552dbccd2d4037a8320626bb781
henryye <yixuy@sfu.ca>
2022-08-02 15:37:53 -0700

    Clean up the game, pull the board out of the gam

--- src/GameLogic/Board.py
+++ src/GameLogic/Board.py
@@ -22,7 +22,7 @@ class Board:
     # Cell Type 0: 1x1 cell
     # Cell Type 1: 1x2 cell
     # Cell Type 2: 2x1 cell
-    def setCellType0(self, x_coord, y_coord):
+    def set_cell_type0(self, x_coord, y_coord):
         self.board[y_coord+1][x_coord] = 0
         self.board[y_coord][x_coord+1] = 0
         self.board[y_coord-1][x_coord] = 0
@@ -32,7 +32,7 @@ class Board:
         self.board[y_coord+1][x_coord-1] = 0
         self.board[y_coord-1][x_coord+1] = 0
 
-    def setCellType1(self, x_coord, y_coord):
+    def set_cell_type1(self, x_coord, y_coord):
         self.board[y_coord+1][x_coord] = -1
         self.board[y_coord+1][x_coord+1] = 0
         self.board[y_coord+1][x_coord-1] = 0
@@ -45,7 +45,7 @@ class Board:
         self.board[y_coord+2][x_coord-1] = 0
         self.board[y_coord-1][x_coord+1] = 0
 
-    def setCellType2(self, x_coord, y_coord):
+    def set_cell_type2(self, x_coord, y_coord):
         self.board[y_coord][x_coord + 1] = -1
         self.board[y_coord+1][x_coord] = 0
         self.board[y_coord][x_coord+2] = 0
@@ -75,14 +75,13 @@ class Board:
             if(random_Y != 0 and random_X != 0 and random_Y != self.rows-1 and random_X != self.columns-1):
                 self.board[random_Y][random_X] = -1
                 if(cellType == 0):
-                    self.setCellType0(random_X, random_Y)
+                    self.set_cell_type0(random_X, random_Y)
                 elif(cellType == 1 and random_Y < self.rows-2):
-                    self.setCellType1(random_X,random_Y)
+                    self.set_cell_type1(random_X,random_Y)
                 elif(cellType == 2 and random_X < self.columns-2):
-                    self.setCellType2(random_X,random_Y)
+                    self.set_cell_type2(random_X,random_Y)
                 else:
                     self.board[random_Y][random_X] = 0
-
 
     def print_board(self):
         print('\n'.join(' '.join('{0: ^3}'.format(str(i)) for i in row) for row in self.board))
